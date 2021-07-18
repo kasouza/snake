ea7efe7b1d9b9ebd87a293e8d340ed8211d50b1a
kaso <kaua.scmm@gmail.com>
2021-07-18 16:06:03 -0300

    fixed some bugs and added the `Game` class

--- index.js
+++ index.js
@@ -13,117 +13,36 @@ const COLS = Math.floor(HEIGHT / SQUARE_SIZE);
 const TIME_FACTOR_PER_FRUIT = 0.99;
 const MIN_ELAPSED_TIME_THRESHOLD = 10;
 
-const commandsStack = [];
-
-function GameObject() {
-  this.grid = new Grid(COLS, ROWS);
-  this.snake = new Snake(Math.round(COLS / 2) - 1, Math.round(ROWS / 2) - 1);
-  this.startTime = 0;
-  this.elapsedTimeThreshold = 150;
-  this.fruitsToCreate = 1;
-  this.collectedFruits = 0;
-}
-
 function setup() {
-  let gameObject = new GameObject();
+  const game = new Game();
 
-  gameObject.grid.randomFruity();
+  // Put the comamnds in the stack so the snake
+  // doesn't need to be a global variable
+  window.addEventListener('keydown', (e) => {
+    switch (e.key) {
+      case 'ArrowUp':
+        e.preventDefault();
+        game.commandsStack.push([0, 1]);
+        break;
 
-  requestAnimationFrame((endTime) => update(gameObject, endTime));
-}
+      case 'ArrowRight':
+        e.preventDefault();
+        game.commandsStack.push([1, 0]);
+        break;
 
-function update(gameObject, endTime) {
-  let hasLost = false;
+      case 'ArrowDown':
+        e.preventDefault();
+        game.commandsStack.push([0, -1]);
+        break;
 
-  draw(gameObject.grid, gameObject.snake);
-
-  // This makes the game slower (in a good way!)
-  elapsed = endTime - gameObject.startTime;
-  if (elapsed >= gameObject.elapsedTimeThreshold) {
-    if (commandsStack.length >= 1) {
-      gameObject.snake.changeDirection(commandsStack.pop());
+      case 'ArrowLeft':
+        e.preventDefault();
+        game.commandsStack.push([-1, 0]);
+        break;
     }
+  });
 
-    let next = gameObject.snake.head;
-    while (next) {
-      const fruit = gameObject.grid.getCollision(next);
-      if (fruit) {
-        gameObject.grid.removeFruit(fruit);
-
-        // Only make the snake grow and create new fruits if a fruit collided
-        // with the head, otherwise just deletes the fruit. This is a little
-        // hack for when a fruit spawn in the tail of the snake, as it will
-        // simply be removed.
-        if (next === gameObject.snake.head) {
-          if (gameObject.grid.fruits.length == 0) {
-            for (let i = 0; i < gameObject.fruitsToCreate; i++) {
-              gameObject.grid.randomFruity();
-            }
-            
-            if (gameObject.collectedFruits % 2 == 0) {
-              gameObject.fruitsToCreate++;
-            }
-          }
-
-          gameObject.snake.grow();
-
-          // Make the game faster when you collect a fruit
-          gameObject.elapsedTimeThreshold = Math.max((gameObject.elapsedTimeThreshold
-            * TIME_FACTOR_PER_FRUIT), MIN_ELAPSED_TIME_THRESHOLD);
-        }
-      }
-
-      next = next.next;
-    }
-
-    gameObject.snake.update();
-
-    // Lost the game
-    hasLost = gameObject.snake.hasLost();
-
-    gameObject.startTime = endTime;
-  }
-
-  if (hasLost) {
-    alert("You Lost!");
-    setup();
-  } else {
-    requestAnimationFrame((endTime) => update(gameObject, endTime));
-  }
+  game.start();
 }
-
-function draw(grid, snake) {
-  ctx.fillStyle = 'black';
-  ctx.fillRect(0, 0, WIDTH, HEIGHT);
-
-  grid.show();
-  snake.show();
-}
-
-// Put the comamnds in the stack so the snake
-// doesn't need to be a global variable
-window.addEventListener('keydown', (e) => {
-  switch (e.key) {
-    case 'ArrowUp':
-      e.preventDefault();
-      commandsStack.push([0, 1]);
-      break;
-
-    case 'ArrowRight':
-      e.preventDefault();
-      commandsStack.push([1, 0]);
-      break;
-
-    case 'ArrowDown':
-      e.preventDefault();
-      commandsStack.push([0, -1]);
-      break;
-
-    case 'ArrowLeft':
-      e.preventDefault();
-      commandsStack.push([-1, 0]);
-      break;
-  }
-});
 
 window.onload = setup;
